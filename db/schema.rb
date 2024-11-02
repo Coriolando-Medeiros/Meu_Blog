d4cbf6ebb8f480fd0fb33b8c8c2ca01e435ad81d
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-11-02 06:33:10 -0300

    feat: modificar a coluna data_publicacao para published_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_02_083844) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_02_091931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title", limit: 150
     t.text "body"
-    t.datetime "data_publicacao"
+    t.datetime "published_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
