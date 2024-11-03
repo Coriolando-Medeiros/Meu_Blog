e2eb7db4c94bd59eed9fc2173702d83eaf02405c
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-11-03 05:45:09 -0300

    feat: adicionar o layout 'application_blank' ao controller

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,7 @@
 class BlogController < ApplicationController
-  def Index
-    @post = post.where("published_date <= ?", Time.zone.now).first
+  layout "application_blank"
+
+  def index
+    @post = Post.where("published_date <= ?", Time.zone.now).order("published_date desc").first
   end
 end
